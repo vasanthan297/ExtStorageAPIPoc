e2b76d8da7236a6c4d8c64a3f89af80bcf843155
vasanthan297 <vasanthan297@gmail.com>
2023-06-10 15:36:27 +0800

    Update index.ts

--- buildandreleasetask/index.ts
+++ buildandreleasetask/index.ts
@@ -1,39 +1,30 @@
 import tl = require('azure-pipelines-task-lib/task');
 import * as azdev from "azure-devops-node-api";
 import { v4 as uuidv4 } from 'uuid';
-
-
 async function run() {	
   try {	
     const docId = uuidv4();
     console.log("create document with Id: ", docId);  
     const organizationUrl = String(tl.getVariable('System.TeamFoundationCollectionUri'));
     let token = String(tl.getVariable('System.AccessToken'));
-    console.log(' ************** Task Init:01 ******************************')    
     const azDevConnection = azdev.WebApi.createWithBearerToken(organizationUrl, token);
     const eManager = await azDevConnection.getExtensionManagementApi();
     const collectionName = 'doc-poc'; // Replace with your collection name
     const documentId = 'doc1'; // Replace with your document ID
     const documentContent = {
-    name: 'My Document',
-      description: 'This is a sample document.',
-      // Add any other properties or content you want for your document
-    };
     var myDoc = {
         id: docId,
         fullScreen: false,
         screenWidth: 500
     };
-	console.log('eManager 1 ', eManager)	
     const document = await eManager.createDocumentByName(
          myDoc,
         'vasanthan297', 
         'build-release-task',
         'Default',
         'Current',
-         collectionName
-       
-      );
+         collectionName       
+      );	  
     console.log('Document created successfully.');	       
     // get  document
     const document2 = await eManager.getDocumentByName(
